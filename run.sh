21bc8b25c0cc3c549daacbf5943ebea023112046
Eoin Houlihan <ehoulih@tcd.ie>
2015-12-11 16:48:36 +0000

    For loops

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ if make; then
   pushd ../TastierMachine
   if cabal install; then
     popd
-    mono bin/tcc.exe test/Programs/condtest.tas test.asm
+    mono bin/tcc.exe test/Programs/fortest.tas test.asm
     if [ -e test.asm ]; then
       if tasm test.asm test.bc; then
         tvm test.bc test/Inputs/test.IN
